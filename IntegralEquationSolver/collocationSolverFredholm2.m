18a31350ccffbc7682adcb511bbc4a5ea7b08810
Molochynskyi Yehor <molochynskyi.e@gmail.com>
2023-04-05 13:39:41 +0300

    Add collocation method

--- IntegralEquationSolver/collocationSolverFredholm2.m
+++ IntegralEquationSolver/collocationSolverFredholm2.m
@@ -1,4 +1,4 @@
-function [X, Y] = collocationSolverFredholm2(lambda, core, a, b, rightFunc, n)    
+function [X, Y] = collocationSolverFredholm2(lambda, core, a, b, rightFunc, n, quadFormula)    
     % u(x) - lambda \int_a^b K(x, t) u(t) dt = f(x)
 
     % TO DO:
@@ -6,24 +6,21 @@ function [X, Y] = collocationSolverFredholm2(lambda, core, a, b, rightFunc, n)
     % - 
 
     % create grid
-    X = getGrid1D(a, b, n);
+    X = linspace(a, b, n);
+    weights = quadFormula(a, b, n);
 
     %
-    A = zeros(length(X) - 1, length(X) - 1);
-    F = zeros(length(X) - 1, 1);
-    for i=1:length(X) - 1
-        for j=1:length(X) - 1
-            node = core(X(i), X(j));
-            weight = (core(X(i), X(j)) + core(X(i), X(j + 1)))/2 * (X(j + 1) - X(j));
-            A(i, j) = -lambda * node * weight;
+    A = zeros(length(X), length(X));
+    F = zeros(length(X), 1);
+    for i=1:length(X)
+        for j=1:length(X)
+            A(i, j) = -lambda * weights(j) * core(X(i), X(j));
         end
         % kronecker
         A(i, i) = 1 + A(i, i);
-        
         F(i, 1) = rightFunc(X(i));
     end
 
     % solve SLR
     Y = linsolve(A, F);
-    X = X(1:end-1);
 end
