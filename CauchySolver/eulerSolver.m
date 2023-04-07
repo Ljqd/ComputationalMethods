1ee73344bbb50f6269a82bc8b6c3acf0053d7722
Molochynskyi Yehor <molochynskyi.e@gmail.com>
2023-04-07 10:56:18 +0300

    Add single-step Cauchy problem solvers

---
+++ CauchySolver/eulerSolver.m
@@ -0,0 +1,86 @@
+function [X, Y] = eulerSolver(dfunc, x0, u0, xn, n, side)
+    if x0 >= xn
+        error("eulerSolver: x0 >= xn");
+    end
+
+
+    if side == SideEnum.Left
+        [X, Y] = eulerSolverLeft(dfunc, x0, u0, xn, n);
+        return
+    end
+
+    if side == SideEnum.Center
+        [X, Y] = eulerSolverCenter(dfunc, x0, u0, xn, n);
+        return
+    end
+    
+    [X, Y] = eulerSolverRight(dfunc, x0, u0, xn, n);
+end
+
+function [X, Y] = eulerSolverLeft(dfunc, x0, u0, xn, n)
+    dx = (xn - x0) / n;
+    X = x0:dx:xn;
+    Y = u0;
+
+    y0 = u0;
+    for i=2:length(X)
+        y1 = y0 + dx * dfunc(x0, y0);
+        x0 = x0 + dx;
+        y0 = y1;
+        Y(i) = y0;
+    end
+end
+
+function [X, Y] = eulerSolverCenter(dfunc, x0, u0, xn, n)
+    dx = (xn - x0) / n;
+    X = linspace(x0, xn, n);
+    Y = u0;
+
+    y0 = u0;
+    for i=2:length(X)
+
+        if i == 2
+            y1 = y0;
+        else
+            F = @(y)(y - dx*(dfunc(x0, y0) + dfunc(x0 + dx, y))/2 - y0);
+            y1 = newton(F, y0);
+        end
+
+        y0 = y1;
+        x0 = x0 + dx;
+        Y(i) = y0;
+    end
+end
+
+function [X, Y] = eulerSolverRight(dfunc, x0, u0, xn, n)
+    dx = (xn - x0) / n;
+    X = linspace(x0, xn, n);
+    Y = u0;
+
+    y0 = u0;
+    for i=2:length(X)
+        x0 = x0 + dx;
+
+        if i == 2
+            y1 = y0;
+        else
+            F = @(y)(y - dx*dfunc(x0, y) - y0);
+            y1 = newton(F, y0);
+        end
+
+        y0 = y1;
+        Y(i) = y0;
+    end
+end
+
+function y0 = newton(f, y0)
+    eps = 10^(-9);
+
+    depth = 100; k = 0;
+    while f(y0) ~= 0 && k < depth 
+        df = (f(y0 + eps) - f(y0)) / eps;
+
+        y0 = y0 - f(y0) / df;
+        k = k + 1;
+    end
+end
